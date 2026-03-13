fc728e2f47fc84067381f19a5ea1eee8bc68c0eb
Erdenetugs Enkhchuluun <eenkhchuluun@akumoproject.com>
2026-03-12 22:27:35 -0500

    hash

--- scp/main.tf
+++ scp/main.tf
@@ -1,10 +1,10 @@
 module "scp" {
-  source = "git::https://github.com/ochirdorj/service_control_policy.git?ref=1887c19a1b2c3d4e5f6a1b2c3d4e5f6a1b2c3d4e"
+  source = "git::https://github.com/ochirdorj/service_control_policy.git?ref=1887c1948c1e46c69ff9eee744aa8c5c8f98c510"
 
   ##Input variables##
   scp_name = "EnforceTag"
   scp_description = "SCP to enforce resource tag"
-  scp_json_path = file("../policies/tag_enforce_policy.json")
+  scp_json_path = file("${path.module}/policies/tag_enforce_policy.json")
   scp_type = "SERVICE_CONTROL_POLICY"
   include_root = false
   target_ou_names = ["Security"]

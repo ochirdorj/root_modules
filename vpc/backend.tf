a2ae08199b10c0861ab46ca8047e4e99407b4f5c
Erdenetugs Enkhchuluun <eenkhchuluun@akumoproject.com>
2026-03-14 18:44:43 -0500

    state file recovery

--- vpc/backend.tf
+++ vpc/backend.tf
@@ -1,7 +1,7 @@
 terraform {
   backend "s3" {
     bucket = "ochirdorj-terraform-backend-bucket"
-    key = "infra/example_s3_bucket/terraform.tfstate"
+    key = "infra/vpc_backend/terraform.tfstate"
     region = "us-east-1"
     encrypt = true
   }

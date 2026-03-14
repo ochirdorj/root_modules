90c0392540d2fa786def4a1dd6ea40d61f884ca2
Erdenetugs Enkhchuluun <eenkhchuluun@akumoproject.com>
2026-03-14 16:27:32 -0500

    git name fix

--- vpc/main.tf
+++ vpc/main.tf
@@ -1,5 +1,5 @@
 module "vpc" {
-  source = "git::https://github.com/ochirdorj/vpc.git?ref=d595bc1ca2222b805335966b15ff164fcf71a1a4"
+  source = "git::https://github.com/ochirdorj/vpc_module.git?ref=d595bc1ca2222b805335966b15ff164fcf71a1a4"
 
   project              = var.project
   vpc_cidr             = var.vpc_cidr

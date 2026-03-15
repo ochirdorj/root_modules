ce5ddf0b9378076a15ee4f8b69647752f88dcd8d
Erdenetugs Enkhchuluun <eenkhchuluun@akumoproject.com>
2026-03-14 20:58:13 -0500

    variable file arrived

--- scp/main.tf
+++ scp/main.tf
@@ -2,11 +2,11 @@ module "scp" {
   source = "git::https://github.com/ochirdorj/service_control_policy.git?ref=1bb18b9d7478305c03ef57c3e5c63a8a47908515"
 
   ##Input variables##
-  scp_name = "EnforceTag"
-  scp_description = "SCP to enforce resource tag"
-  scp_path = "${path.module}/policies/tag_enforce_policy.json"
-  scp_type = "SERVICE_CONTROL_POLICY"
-  include_root = false
-  target_ou_names = ["Security"]
-  target_account_names = []
+  scp_name = var.scp_name
+  scp_description = var.scp_description
+  scp_path = var.scp_path
+  scp_type = var.scp_type
+  include_root = var.include_root
+  target_ou_names = var.target_ou_names
+  target_account_names = var.target_account_names
 }
---
+++ scp/variables.tf
@@ -0,0 +1,38 @@
+variable "scp_name" {
+  type = string
+  default = "EnforceTag"
+}
+
+variable "scp_description" {
+  type = string
+  default = "SCP to enforce resource tag"
+}
+
+variable "scp_path" {
+  type = string
+  default = "${path.module}/policies/tag_enforce_policy.json"
+}
+
+variable "scp_type" {
+  type = string
+  default = "SERVICE_CONTROL_POLICY"
+}
+
+variable "include_root" {
+  type = bool
+  description = "include root_id = true, exlclude root_id = false"
+  default = false
+}
+
+variable "target_ou_names" {
+  type = list(string)
+  description = "scp target ou name, leave it blank, it you don't want to attach scp to ou"
+  default = [ "Security" ]
+}
+
+variable "target_account_names" {
+  type = list(string)
+  description = "scp target account names, leave it blank, it you don't want to attach scp to account"
+  
+}
+

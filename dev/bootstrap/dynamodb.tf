a4b4e01e574d49d92b74fa05947a5372f84b5e87
Erdenetugs Enkhchuluun <eenkhchuluun@akumoproject.com>
2026-04-08 17:41:02 -0500

    terraform fmt

--- dev/bootstrap/dynamodb.tf
+++ dev/bootstrap/dynamodb.tf
@@ -3,11 +3,11 @@
 module "dynamodb" {
   source = "git::https://github.com/ochirdorj/infra-core-database-dynamodb-template.git?ref=79e254da7d49358149d5a343af29133ebece73d4"
 
-  table_name  = var.table_name
+  table_name   = var.table_name
   billing_mode = var.billing_mode
-  hash_key    = var.hash_key
-  enable_cmk  = var.enable_cmk
-  enable_pitr = var.enable_pitr
+  hash_key     = var.hash_key
+  enable_cmk   = var.enable_cmk
+  enable_pitr  = var.enable_pitr
 
   tag_Environment = var.tag_Environment
   tag_Managed_By  = var.tag_Managed_By

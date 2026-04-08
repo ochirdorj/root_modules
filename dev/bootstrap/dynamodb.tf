b3a4cd62139a0bf9c75fb1b1f1d4147c0b305029
Erdenetugs Enkhchuluun <eenkhchuluun@akumoproject.com>
2026-04-08 17:55:47 -0500

    duplication of dynamodb.tf

--- dev/bootstrap/dynamodb.tf
+++ dev/bootstrap/dynamodb.tf
@@ -1,7 +1,7 @@
 # DYNAMODB — TERRAFORM STATE LOCK TABLE
 
 module "dynamodb" {
-  source = "git::https://github.com/ochirdorj/infra-core-database-dynamodb-template.git?ref=cbfaed659304dafc78f6dfc6091f60e182234e4a"
+  source = "git::https://github.com/ochirdorj/infra-core-database-dynamodb-template.git?ref=1b953cbe45825f9082eadc3bba63fad4521fd027"
 
   table_name   = var.table_name
   billing_mode = var.billing_mode
